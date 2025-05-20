179879162f8501fae8ded872339c02b83b34ec2f
quentinljones <quentinljones97@gmail.com>
2025-05-19 22:53:37 -0400

    Add project files.

---
+++ HiItsMeQuentinLJones/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HiItsMeQuentinLJones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
